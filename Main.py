40871721afbeca0acfdc7898ba5d8e1c4898cfc3
Gustav Holmström <gustav.holmstroem@gmail.com>
2023-05-16 10:37:11 +0200

    grund-loop och struktur

--- Main.py
+++ Main.py
@@ -1,1 +1,33 @@
-print("hello world!")
+import pygame
+import numpy
+from pygame.locals import QUIT
+
+run = True
+width = 800
+height = 800
+display = pygame.display.set_mode((width, height))
+
+
+def handleInput():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            global run
+            run = False
+
+
+def update():
+    return
+
+
+def render():
+    return
+
+
+def program():
+    while run:
+        handleInput()
+        update()
+        render()
+    pygame.quit()
+
+program()
