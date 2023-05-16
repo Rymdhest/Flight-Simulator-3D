2b527f87512be8365d4310fc8ad81f186823fb75
Gustav Holmström <gustav.holmstroem@gmail.com>
2023-05-16 11:55:23 +0200

    första triangeln!

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
+import numpy as np
 import pygame
-import numpy
+import time
 from pygame.locals import QUIT
+from pygame.math import clamp
+from numpy import array
+from numpy import cos
+from numpy import sin
+
 
 run = True
 width = 800
@@ -20,8 +26,17 @@ def update():
 
 
 def render():
-    return
+    display.fill([255, 255, 255])
 
+    vertices = array([[400.0, 400.0], [600.0, 650.0], [500.0, 400.0]])
+    vertices[2] += sin(time.time())*100
+    color = array([255, 0, 0])
+    color[2] = 155+cos(time.time())*100
+    color[2] = clamp(color[2], 0 , 255)
+    pygame.draw.polygon(display, color, vertices)
+
+    pygame.display.update()
+    pygame.display.flip()
 
 def program():
     while run:
